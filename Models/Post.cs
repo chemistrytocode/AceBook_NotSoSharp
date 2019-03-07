ba1a778e1000af871acf6b16043d038c6e09d96e
Alex Hutchings <alexhutchings4@gmail.com>
2019-03-07 15:26:14 +0000

    Add Models for Posts

---
+++ Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace AcebookAPI.Models
+{
+    public class Post
+    {
+        public long Id { get; set; }
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
